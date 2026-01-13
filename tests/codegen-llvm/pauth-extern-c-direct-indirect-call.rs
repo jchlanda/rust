e69a5e5f7284838c85cde654480724a0fccf5aa6
Jakub Chlanda <jchlanda@accesssoftek.com>
2026-01-13 14:38:05 +0000

    Update FileCheck's directives

--- tests/codegen-llvm/pauth-extern-c-direct-indirect-call.rs
+++ tests/codegen-llvm/pauth-extern-c-direct-indirect-call.rs
@@ -1,15 +1,26 @@
 //@ revisions: O0_PAUTH O3_PAUTH
 
+// FIXME: JKB: This should really be aarch64 pauthtest
+//@ [O0_PAUTH] needs-llvm-components: aarch64
 //@ [O0_PAUTH] compile-flags: --target=aarch64-unknown-linux-pauthtest -Z pauth -C opt-level=0
+//@ [O3_PAUTH] needs-llvm-components: aarch64
 //@ [O3_PAUTH] compile-flags: --target=aarch64-unknown-linux-pauthtest -Z pauth -C opt-level=3
 
-// Make sure that direct extern "C" calls are not handled by pointer authentication logic.
+// Make sure that direct extern "C" calls are not handled by pointer authentication operand bundle
+// logic.
+use std::ffi::c_void;
 use std::hint::black_box;
 
 extern "C" {
     fn rand() -> i32;
     fn add(a: i32, b: i32) -> i32;
     fn sub(a: i32, b: i32) -> i32;
+
+    // Corresponds to: void *woof;
+    static mut woof: *mut c_void;
+    fn direct_function_taking_void_arg(data: *mut c_void);
+    fn direct_no_arg();
+    fn direct_function_taking_fp_arg(func: unsafe extern "C" fn());
 }
 
 type CFnPtr = unsafe extern "C" fn(i32, i32) -> i32;
@@ -21,18 +32,29 @@ fn test_indirect_call() {
     let fp_sub: CFnPtr = black_box(sub);
 
     unsafe {
+        // O0_PAUTH: call {{.*}}i32 %fp_add({{.*}}) #{{[0-9]+}} [ "ptrauth"(i32{{.*}}, i64{{.*}}) ]
         // O3_PAUTH: call {{.*}}i32 %fp_add({{.*}}) #{{[0-9]+}} [ "ptrauth"(i32{{.*}}, i64{{.*}}) ]
         let _id1 = fp_add(7, 4);
+        // O0_PAUTH: call {{.*}}i32 %fp_sub({{.*}}) #{{[0-9]+}} [ "ptrauth"(i32{{.*}}, i64{{.*}}) ]
         // O3_PAUTH: call {{.*}}i32 %fp_sub({{.*}}) #{{[0-9]+}} [ "ptrauth"(i32{{.*}}, i64{{.*}}) ]
         let _id2 = fp_sub(10, 6);
     }
 
     // Also test calling via conditional pointer
     unsafe {
+        // O0_PAUTH: @rand
+        // O0_PAUTH-NOT: [ "ptrauth"(i32{{.*}}, i64{{.*}}) ]
+        // O3_PAUTH: @rand
+        // O3_PAUTH-NOT: [ "ptrauth"(i32{{.*}}, i64{{.*}}) ]
         let use_add = rand() % 2 == 0;
         let fp: CFnPtr = if use_add { add } else { sub };
-        // O3_PAUTH: call {{.*}}i32 %add.sub({{.*}}) #{{[0-9]+}} [ "ptrauth"(i32{{.*}}, i64{{.*}}) ]
+        // O0_PAUTH: call {{.*}}i32 %{{.*}}({{.*}}) #{{[0-9]+}} [ "ptrauth"(i32{{.*}}, i64{{.*}}) ]
+        // O3_PAUTH: call {{.*}}i32 %{{.*}}({{.*}}) #{{[0-9]+}} [ "ptrauth"(i32{{.*}}, i64{{.*}}) ]
         let _id3 = fp(1, 2);
+    }
+
+    unsafe {
+        direct_function_taking_fp_arg(direct_no_arg);
     }
 }
 
@@ -40,19 +62,24 @@ fn test_indirect_call() {
 #[inline(never)]
 fn test_direct_call() {
     unsafe {
-        // O0_PAUTH: call i32 @add(
-        // O0_PAUTH-NOT: "ptrauth"(
-        // O3_PAUTH: call {{.*}}i32 @add(
-        // O3_PAUTH-NOT: "ptrauth"(
+        // O0_PAUTH: @add
+        // O0_PAUTH-NOT: [ "ptrauth"(i32{{.*}}, i64{{.*}}) ]
+        // O3_PAUTH: @add
+        // O3_PAUTH-NOT: [ "ptrauth"(i32{{.*}}, i64{{.*}}) ]
         let _d1 = add(2, 3);
-        // O0_PAUTH: call i32 @sub(
-        // O0_PAUTH-NOT: "ptrauth"(
-        // O3_PAUTH: call {{.*}}i32 @sub(
-        // O3_PAUTH-NOT: "ptrauth"(
+        // O0_PAUTH: @sub
+        // O0_PAUTH-NOT: [ "ptrauth"(i32{{.*}}, i64{{.*}}) ]
+        // O3_PAUTH: @sub
+        // O3_PAUTH-NOT: [ "ptrauth"(i32{{.*}}, i64{{.*}}) ]
         let _d2 = sub(5, 1);
+
+        // O0_PAUTH: @direct_function_taking_void_arg
+        // O0_PAUTH-NOT: [ "ptrauth"(i32{{.*}}, i64{{.*}}) ]
+        // O3_PAUTH: direct_function_taking_void_arg
+        // O3_PAUTH-NOT: [ "ptrauth"(i32{{.*}}, i64{{.*}}) ]
+        direct_function_taking_void_arg(woof);
     }
 }
-
 
 fn main() {
     test_indirect_call();
--- tests/codegen-llvm/pauth-extern-c.rs
+++ tests/codegen-llvm/pauth-extern-c.rs
@@ -1,23 +1,29 @@
 //@ revisions: O0_PAUTH O3_PAUTH O0_NO_PAUTH O3_NO_PAUTH
 
-//@ [O0_PAUTH] compile-flags: -Z pauth -C opt-level=0
-//@ [O3_PAUTH] compile-flags: -Z pauth -C opt-level=3
-//@ [O0_NO_PAUTH] compile-flags: -C opt-level=0
-//@ [O3_NO_PAUTH] compile-flags: -C opt-level=3
+// FIXME: JKB: This should really be aarch64 pauthtest
+//@ [O0_PAUTH] needs-llvm-components: aarch64
+//@ [O0_PAUTH] compile-flags: --target=aarch64-unknown-linux-pauthtest -Z pauth -C opt-level=0
+//@ [O3_PAUTH] needs-llvm-components: aarch64
+//@ [O3_PAUTH] compile-flags: --target=aarch64-unknown-linux-pauthtest -Z pauth -C opt-level=3
+//@ [O0_NO_PAUTH] needs-llvm-components: aarch64
+//@ [O0_NO_PAUTH] compile-flags: --target=aarch64-unknown-linux-gnu -C opt-level=0
+//@ [O3_NO_PAUTH] needs-llvm-components: aarch64
+//@ [O3_NO_PAUTH] compile-flags: --target=aarch64-unknown-linux-gnu -C opt-level=3
+
+use std::hint::black_box;
 
 type FnPtr = unsafe extern "C" fn(i32, i32) -> i32;
 
-// O0_NO_PAUTH-NOT: "ptrauth"(i32 0, i64 0)
-// O3_NO_PAUTH-NOT: "ptrauth"(i32 0, i64 0)
-
-// O0_PAUTH: personality ptr ptrauth (ptr @rust_eh_personality, i32 0)
-// O3_PAUTH: personality ptr ptrauth (ptr @rust_eh_personality, i32 0)
+// O0_NO_PAUTH-NOT: "ptrauth"(i32
+// O3_NO_PAUTH-NOT: "ptrauth"(i32
 
 // O0_PAUTH: define {{.*}}pauth_extern_c4main
 // O3_PAUTH: define {{.*}}pauth_extern_c4main
 fn main() {
-    let add_ptr: FnPtr = add_from_c;
-    // O0_PAUTH: call i32 @{{.*}}pauth_extern_c7call_it{{.*}}(ptr ptrauth (ptr @add_from_c, i32 0)
+    // O0_PAUTH: ptr ptrauth (ptr @add_from_c, i32 0)
+    // O3_PAUTH: ptr ptrauth (ptr @add_from_c, i32 0)
+    let add_ptr: FnPtr = black_box(add_from_c);
+    // O0_PAUTH: call i32 @{{.*}}pauth_extern_c7call_it{{.*}}(ptr {{.*}})
     let _sum = call_it(add_ptr, 5, 7);
     assert!(12 == _sum);
 }
@@ -27,15 +33,10 @@ fn main() {
 #[inline(never)]
 fn call_it(fn_ptr: FnPtr, arg_1: i32, arg_2: i32) -> i32 {
     // O0_PAUTH: call i32 %fn_ptr(i32 %arg_1, i32 %arg_2) {{.*}} [ "ptrauth"(i32 0, i64 0) ]
-    // O3_PAUTH: call {{.*}} i32 ptrauth (ptr @add_from_c, i32 0){{.*}} [ "ptrauth"(i32 0, i64 0) ]
+    // O3_PAUTH: call {{.*}} i32 %fn_ptr(i32 {{.*}}, i32 {{.*}}) {{.*}} [ "ptrauth"(i32 0, i64 0) ]
     unsafe { fn_ptr(arg_1, arg_2) }
 }
 
-// O0_PAUTH: declare{{.*}} i32 @rust_eh_personality{{.*}} #[[ATTR_O0_2:[0-9]+]]
-// O3_PAUTH: declare{{.*}} i32 @rust_eh_personality{{.*}} #[[ATTR_O3_2:[0-9]+]]
-
-// O0_PAUTH: declare{{.*}} i32 @add_from_c(i32{{.*}}, i32{{.*}}){{.*}} #[[ATTR_O0_2]]
-// O3_PAUTH: declare{{.*}} i32 @add_from_c(i32{{.*}}, i32{{.*}}){{.*}} #[[ATTR_O3_2]]
 extern "C" {
     fn add_from_c(a: i32, b: i32) -> i32;
 }
@@ -45,14 +46,3 @@ extern "C" {
 // O0_PAUTH: {{.*}}"target-features"="{{.*}}+pauth{{.*}}"
 // O3_PAUTH: attributes #[[ATTR_O3_1]] = { {{.*}}ptrauth-calls
 // O3_PAUTH: {{.*}}"target-features"="{{.*}}+pauth{{.*}}"
-
-// O0_PAUTH: attributes #[[ATTR_O0_2]] = { {{.*}}ptrauth-calls
-// O0_PAUTH: {{.*}}"target-features"="{{.*}}+pauth{{.*}}"
-// O3_PAUTH: attributes #[[ATTR_O3_2]] = { {{.*}}ptrauth-calls
-// O3_PAUTH: {{.*}}"target-features"="{{.*}}+pauth{{.*}}"
-
-// O0_PAUTH: !{i32 {{[0-9]+}}, !"ptrauth-sign-personality", i32 1}
-// O3_PAUTH: !{i32 {{[0-9]+}}, !"ptrauth-sign-personality", i32 1}
-
-// O0_NO_PAUTH-NOT: ptrauth-sign-personality
-// O3_NO_PAUTH-NOT: ptrauth-sign-personality
