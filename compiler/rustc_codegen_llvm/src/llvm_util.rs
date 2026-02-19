e420c7139c20fd2030d989cde382f4c1bea2fee3
Jakub Chlanda <jchlanda@accesssoftek.com>
2026-02-19 08:28:21 +0000

    Update reliable float config - PAC musl does not support 128fp
    Also retire adding `+pauth` feature, as it is now handled by the
    `pauthtest` target description.

--- compiler/rustc_codegen_llvm/src/llvm_util.rs
+++ compiler/rustc_codegen_llvm/src/llvm_util.rs
@@ -386,6 +386,8 @@ fn update_target_reliable_float_cfg(sess: &Session, cfg: &mut TargetConfig) {
     cfg.has_reliable_f128 = match (target_arch, target_os) {
         // Unsupported https://github.com/llvm/llvm-project/issues/121122
         (Arch::AmdGpu, _) => false,
+        // Pauthtest musl does not support 128-bit floating point math.
+        (Arch::AArch64, _) if *target_env == Env::Pauthtest => false,
         // Unsupported <https://github.com/llvm/llvm-project/issues/94434>
         (Arch::Arm64EC, _) => false,
         // Selection bug <https://github.com/llvm/llvm-project/issues/95471>. This issue is closed
@@ -631,11 +633,6 @@ fn llvm_features_by_flags(sess: &Session, features: &mut Vec<String>) {
         } else {
             features.push("+reserve-x18".into());
         }
-    }
-    // -Z pauth
-    if sess.opts.unstable_opts.pauth {
-        // FIXME: JAKUB: Limit all uses of opts.unstable_opts.pauth to AArch64.
-        features.push("+pauth".into());
     }
 }
 
