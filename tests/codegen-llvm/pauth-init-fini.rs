1ebfe2ae7120f6adb179168ce9054d94a4e1b4cf
Jakub Chlanda <jchlanda@accesssoftek.com>
2026-03-18 12:17:17 +0000

    Introduce pauth_disable_init_fini_addr_discriminator option
    And update tests to reflect the use of address diversity for init/fini

--- tests/codegen-llvm/pauth-init-fini.rs
+++ tests/codegen-llvm/pauth-init-fini.rs
@@ -1,15 +1,15 @@
 // ignore-tidy-linelength
 //@ only-aarch64-unknown-linux-pauthtest
-//@ revisions: O0_PAUTH O3_PAUTH O0_PAUTH_DISABLE_SIGN_INIT_FINI O3_PAUTH_DISABLE_SIGN_INIT_FINI
+//@ revisions: O0_PAUTH O3_PAUTH O0_PAUTH_DISABLE_INIT_FINI_ADDR_DISC O3_PAUTH_DISABLE_INIT_FINI_ADDR_DISC
 
 //@ [O0_PAUTH] needs-llvm-components: aarch64
 //@ [O0_PAUTH] compile-flags: --target=aarch64-unknown-linux-pauthtest -C opt-level=0
 //@ [O3_PAUTH] needs-llvm-components: aarch64
 //@ [O3_PAUTH] compile-flags: --target=aarch64-unknown-linux-pauthtest -C opt-level=3
-//@ [O0_PAUTH_DISABLE_SIGN_INIT_FINI] needs-llvm-components: aarch64
-//@ [O0_PAUTH_DISABLE_SIGN_INIT_FINI] compile-flags: --target=aarch64-unknown-linux-pauthtest -Z pauth_disable_sign_init_fini -C opt-level=0
-//@ [O3_PAUTH_DISABLE_SIGN_INIT_FINI] needs-llvm-components: aarch64
-//@ [O3_PAUTH_DISABLE_SIGN_INIT_FINI] compile-flags: --target=aarch64-unknown-linux-pauthtest -Z pauth_disable_sign_init_fini -C opt-level=3
+//@ [O0_PAUTH_DISABLE_INIT_FINI_ADDR_DISC] needs-llvm-components: aarch64
+//@ [O0_PAUTH_DISABLE_INIT_FINI_ADDR_DISC] compile-flags: --target=aarch64-unknown-linux-pauthtest -Z pauth_disable_init_fini_addr_discriminator -C opt-level=0
+//@ [O3_PAUTH_DISABLE_INIT_FINI_ADDR_DISC] needs-llvm-components: aarch64
+//@ [O3_PAUTH_DISABLE_INIT_FINI_ADDR_DISC] compile-flags: --target=aarch64-unknown-linux-pauthtest -Z pauth_disable_init_fini_addr_discriminator -C opt-level=3
 
 // Make sure that init/fini metadata uses correct discriminator: 0xd9d4/55764
 
@@ -18,18 +18,19 @@
 
 #[used]
 #[link_section = ".init_array.90"]
-// O0_PAUTH: @{{[0-9A-Za-z_]+}}GLOBAL_INIT = constant ptr ptrauth (ptr @{{[0-9A-Za-z_]+}}init_fn, i32 0, i64 55764), section ".init_array.90"
-// O3_PAUTH: @{{[0-9A-Za-z_]+}}GLOBAL_INIT = constant ptr ptrauth (ptr @{{[0-9A-Za-z_]+}}init_fn, i32 0, i64 55764), section ".init_array.90"
-// O0_PAUTH_DISABLE_SIGN_INIT_FINI: @{{[0-9A-Za-z_]+}}GLOBAL_INIT = constant ptr @{{[0-9A-Za-z_]+}}init_fn, section ".init_array.90"
-// O3_PAUTH_DISABLE_SIGN_INIT_FINI: @{{[0-9A-Za-z_]+}}GLOBAL_INIT = constant ptr @{{[0-9A-Za-z_]+}}init_fn, section ".init_array.90"
+// O0_PAUTH: @{{[0-9A-Za-z_]+}}GLOBAL_INIT = constant ptr ptrauth (ptr @{{[0-9A-Za-z_]+}}init_fn, i32 0, i64 55764, ptr inttoptr (i64 1 to ptr)), section ".init_array.90"
+// O3_PAUTH: @{{[0-9A-Za-z_]+}}GLOBAL_INIT = constant ptr ptrauth (ptr @{{[0-9A-Za-z_]+}}init_fn, i32 0, i64 55764, ptr inttoptr (i64 1 to ptr)), section ".init_array.90"
+// O0_PAUTH_DISABLE_INIT_FINI_ADDR_DISC: @{{[0-9A-Za-z_]+}}GLOBAL_INIT = constant ptr ptrauth (ptr @{{[0-9A-Za-z_]+}}init_fn, i32 0, i64 55764), section ".init_array.90"
+// O3_PAUTH_DISABLE_INIT_FINI_ADDR_DISC: @{{[0-9A-Za-z_]+}}GLOBAL_INIT = constant ptr ptrauth (ptr @{{[0-9A-Za-z_]+}}init_fn, i32 0, i64 55764), section ".init_array.90"
+
 static GLOBAL_INIT: extern "C" fn() = init_fn;
 
 #[used]
 #[link_section = ".fini_array.90"]
-// O0_PAUTH: @{{[0-9A-Za-z_]+}}GLOBAL_FINI = constant ptr ptrauth (ptr @{{[0-9A-Za-z_]+}}fini_fn, i32 0, i64 55764), section ".fini_array.90"
-// O3_PAUTH: @{{[0-9A-Za-z_]+}}GLOBAL_FINI = constant ptr ptrauth (ptr @{{[0-9A-Za-z_]+}}fini_fn, i32 0, i64 55764), section ".fini_array.90"
-// O0_PAUTH_DISABLE_SIGN_INIT_FINI: @{{[0-9A-Za-z_]+}}GLOBAL_FINI = constant ptr @{{[0-9A-Za-z_]+}}fini_fn, section ".fini_array.90"
-// O3_PAUTH_DISABLE_SIGN_INIT_FINI: @{{[0-9A-Za-z_]+}}GLOBAL_FINI = constant ptr @{{[0-9A-Za-z_]+}}fini_fn, section ".fini_array.90"
+// O0_PAUTH: @{{[0-9A-Za-z_]+}}GLOBAL_FINI = constant ptr ptrauth (ptr @{{[0-9A-Za-z_]+}}fini_fn, i32 0, i64 55764, ptr inttoptr (i64 1 to ptr)), section ".fini_array.90"
+// O3_PAUTH: @{{[0-9A-Za-z_]+}}GLOBAL_FINI = constant ptr ptrauth (ptr @{{[0-9A-Za-z_]+}}fini_fn, i32 0, i64 55764, ptr inttoptr (i64 1 to ptr)), section ".fini_array.90"
+// O0_PAUTH_DISABLE_INIT_FINI_ADDR_DISC: @{{[0-9A-Za-z_]+}}GLOBAL_FINI = constant ptr ptrauth (ptr @{{[0-9A-Za-z_]+}}fini_fn, i32 0, i64 55764), section ".fini_array.90"
+// O3_PAUTH_DISABLE_INIT_FINI_ADDR_DISC: @{{[0-9A-Za-z_]+}}GLOBAL_FINI = constant ptr ptrauth (ptr @{{[0-9A-Za-z_]+}}fini_fn, i32 0, i64 55764), section ".fini_array.90"
 static GLOBAL_FINI: extern "C" fn() = fini_fn;
 
 extern "C" fn init_fn() {}
