edf2b61c931ee32a178717cc53c51702e11a27fd
Jakub Chlanda <jchlanda@accesssoftek.com>
2026-03-27 10:57:36 +0000

    Format

--- library/unwind/src/lib.rs
+++ library/unwind/src/lib.rs
@@ -98,14 +98,14 @@ cfg_select! {
 // musl, the only supported unwinding mechanism is provided by libunwind.
 #[cfg(target_env = "pauthtest")]
 cfg_select! {
-    target_feature = "crt-static" => {
-        compile_error!("pauthtest target only supports dynamic linking to `unwind`");
-    }
-    _ => {
-        #[link(name = "unwind")]
-         unsafe extern "C" {}
+   target_feature = "crt-static" => {
+       compile_error!("pauthtest target only supports dynamic linking to `unwind`");
+   }
+   _ => {
+       #[link(name = "unwind")]
+        unsafe extern "C" {}
      }
- }
+}
 
 // This is the same as musl except that we default to using the system libunwind
 // instead of libgcc.
