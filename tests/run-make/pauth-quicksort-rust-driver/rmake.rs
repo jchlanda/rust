f81f36c8d6de6590cc1392190c77cfe9a08c3a39
Jakub Chlanda <jchlanda@accesssoftek.com>
2026-03-27 12:56:33 +0000

    Teach bootstrap how to build `rust_test_helpers` for pauthtest
    Also:
    * update tests to force dynamic library when targetting pauthtest
    * introduce end-to-end tests for pauthtest (in run-make)

---
+++ tests/run-make/pauth-quicksort-rust-driver/rmake.rs
@@ -0,0 +1,34 @@
+// Test compilation flow using custom pauth-enabled toolchain and signing extern "C" function
+// pointers used from withing rust. Note that in order for the test to work the toolchain has to be
+// provided via env variable (LLVM_PAUTH), or present at `/opt/llvm-pauth`.
+// In this test rust is the driver - providing the data and the comparison function; while c -
+// provides the implementation of quicksort algorithm and is the user of  the data and comparator.
+
+//@ only-aarch64-unknown-linux-pauthtest
+
+use run_make_support::{cc, rfs, run, run_fail, rustc};
+
+fn main() {
+    let root = std::env::var("LLVM_PAUTH").unwrap_or_else(|_| "/opt/llvm-pauth".into());
+
+    let clang_path = format!("{}/bin/clang", root);
+    unsafe {
+        std::env::set_var("CC", clang_path);
+    }
+
+    let input = "quicksort";
+    let input_name = format!("{input}.c");
+    let lib_name = format!("{}{input}.{}", "lib", "so");
+    cc().out_exe(&lib_name)
+        .input(&input_name)
+        .args(&["-target", "aarch64-linux-pauthtest", "-fPIC", "-shared"])
+        .run();
+
+    rustc().target("aarch64-unknown-linux-pauthtest").input("main.rs").run();
+    run("main");
+    //rustc().target("aarch64-unknown-linux-pauthtest").input("main.rs")
+    //.args(&["--emit=llvm-ir,asm"]).run();
+
+    rfs::remove_file(&lib_name);
+    run_fail("main");
+}
